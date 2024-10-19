c6c73c3e6e7f3f539a82c5b4e31acd11e17fac03
Adelin <adelin10emil@gmail.com>
2024-10-19 16:16:57 +0300

    Update CurrencyType in Enums.java

--- src/Enums.java
+++ src/Enums.java
@@ -1,4 +1,22 @@
 public class Enums {
-    public enum currencyType {EUR, USD, GBP}
-    public enum transactionType {TRANSFER, WITHDRAW, DEPOSIT}
+    public enum CurrencyType {
+        EUR(1.0),
+        USD(0.84),
+        GBP(1.16);
+
+        private final double conversionRate;
+
+        CurrencyType(double conversionRate){
+            this.conversionRate = conversionRate;
+        }
+
+
+        public double getConversionRate(){
+            return conversionRate;
+        }
+
+    }
+
+
+    public enum TransactionType {TRANSFER, WITHDRAW, DEPOSIT}
 }
