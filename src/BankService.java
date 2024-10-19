789b74107256f839fed83c0f1fc287ee5ab7bf91
Adelin <adelin10emil@gmail.com>
2024-10-19 18:32:29 +0300

    Updated BankService.java

---
+++ src/BankService.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class BankService {
+    private Map<String, Account> accounts = new HashMap<>();
+    private Map<String, Transaction> transactions = new HashMap<>();
+    private FileService fileService = new FileService();
+
+
+    public BankService(){
+        accounts = fileService.loadAccounts();
+        transactions = fileService.loadTransactions();
+    }
+
+
+    public void createAccount(String username, Enums.CurrencyType currency, String IBAN, String pin){
+        Account newAccount = new Account(username, currency, IBAN, pin);
+        accounts.put(IBAN, newAccount);
+        fileService.updateAccount(newAccount);
+
+    }
+
+
+    public void makeTransaction(String transactionID, Account accountFrom, Account accountTo,
+                                double amount, Enums.TransactionType type)
+    {
+        Transaction newTransaction = new Transaction(accountFrom, accountTo, amount, type);
+        transactions.put(transactionID, newTransaction);
+
+        Map<String, Transaction> newTransactionMap = new HashMap<>();
+        newTransactionMap.put(transactionID, newTransaction);
+
+        fileService.updateTransactions(newTransactionMap);
+    }
+}
